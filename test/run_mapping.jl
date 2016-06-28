df2dfef7e0c89f8f6121e37f5bf5854aa7d0dc41
Robin Deits <robin.deits@gmail.com>
2016-06-28 17:21:03 -0400

    add command args

--- test/run_mapping.jl
+++ test/run_mapping.jl
@@ -1,6 +1,7 @@
 using MappingRobots
 using Base.Dates
 
-robot = Robot(Brick("/home/pi/Ev3"))
-local_map = run_mapping(robot, timeout=Second(30))
+timeout = ARGS ? Second(parse(Int, ARGS[1])) : Second(30)
 
+robot = Robot(Brick("/home/pi/Ev3"))
+local_map = run_mapping(robot, timeout=timeout)
