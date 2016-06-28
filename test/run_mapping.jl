5fc14e2b268a31eb2667ff9f3e81c447bb892dcd
Robin Deits <robin.deits@gmail.com>
2016-06-28 17:22:34 -0400

    bool for real

--- test/run_mapping.jl
+++ test/run_mapping.jl
@@ -1,7 +1,7 @@
 using MappingRobots
 using Base.Dates
 
-timeout = length(ARGS) ? Second(parse(Int, ARGS[1])) : Second(30)
+timeout = length(ARGS) > 0 ? Second(parse(Int, ARGS[1])) : Second(30)
 
 robot = Robot(Brick("/home/pi/Ev3"))
 local_map = run_mapping(robot, timeout=timeout)
